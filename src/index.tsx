3c9954088fd3081b0863f5f9afa3aa41df8cc828
Koochr <dm.girenko@gmail.com>
2021-05-27 18:01:47 +0300

    eslint stuff

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import {firebaseConfig} from "./firebase/config"
 // TODO
 declare global {
 	interface Window {
-		// eslint-disable-next-line
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
 		ethereum: any
 	}
 }
