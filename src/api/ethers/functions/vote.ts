3612b922cc1765aff20c7634323e2c2f6fe25c01
Koochr <dm.girenko@gmail.com>
2021-06-17 12:02:27 +0300

    Add vote API

---
+++ src/api/ethers/functions/vote.ts
@@ -0,0 +1,25 @@
+import {JsonRpcSigner, Web3Provider} from "@ethersproject/providers"
+import {Contract} from "@ethersproject/contracts"
+import HouseTokenDAO from "../abis/HouseTokenDAO.json"
+
+const vote = (
+	daoAddress: string,
+	proposalId: number,
+	yes: boolean,
+	provider: Web3Provider,
+	signer: JsonRpcSigner
+): Promise<void> =>
+	new Promise<void>(async (resolve, reject) => {
+		try {
+			const daoContract = new Contract(daoAddress, HouseTokenDAO.abi, signer)
+			const tx = await daoContract.vote(proposalId, yes)
+
+			provider.once(tx.hash, () => {
+				resolve()
+			})
+		} catch (e) {
+			reject(e)
+		}
+	})
+
+export default vote
