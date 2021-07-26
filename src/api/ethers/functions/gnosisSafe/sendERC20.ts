be19fbd3af076ea82b81d1fdc4325807e6fe89c9
nginnever <ginneversource@gmail.com>
2021-07-26 09:02:06 -0700

    safe send erc20 api

--- src/api/ethers/functions/gnosisSafe/sendERC20.ts
+++ src/api/ethers/functions/gnosisSafe/sendERC20.ts
@@ -1,35 +1,35 @@
 import {JsonRpcSigner} from "@ethersproject/providers"
 import {Contract} from "@ethersproject/contracts"
 import GnosisSafeL2 from "../../abis/GnosisSafeL2.json"
-import MultiArtToken from "../../abis/MultiArtToken.json"
+import ERC20 from "../../abis/GovToken.json"
 import {buildContractCall, executeTx, SafeSignature, safeSignMessage} from "./safeUtils"
 
-export const signSendNFT = async (
+export const signSendERC20 = async (
 	safeAddress: string,
-	nftID: number,
-	nftAddress: string,
+	amount: number,
+	erc20Address: string,
 	to: string,
 	signer: JsonRpcSigner
 ): Promise<SafeSignature> => {
 	const safeContract = new Contract(safeAddress, GnosisSafeL2.abi, signer)
-	const nft = new Contract(nftAddress, MultiArtToken.abi, signer)
+	const erc20 = new Contract(erc20Address, ERC20.abi, signer)
 	const nonce = await safeContract.nonce()
-	const call = buildContractCall(nft, "transferFrom", [safeAddress, to, nftID], nonce)
+	const call = buildContractCall(erc20, "transfer", [to, amount], nonce)
 	return safeSignMessage(signer, safeContract, call)
 }
 
-export const executeSendNFT = async (
+export const executeSendERC20 = async (
 	safeAddress: string,
-	nftID: number,
-	nftAddress: string,
+	amount: number,
+	erc20Address: string,
 	to: string,
 	signatures: SafeSignature[],
 	signer: JsonRpcSigner
 ): Promise<void> => {
 	const safeContract = new Contract(safeAddress, GnosisSafeL2.abi, signer)
-	const nft = new Contract(nftAddress, MultiArtToken.abi, signer)
+	const erc20 = new Contract(erc20Address, ERC20.abi, signer)
 	const nonce = await safeContract.nonce()
-	const call = buildContractCall(nft, "transferFrom", [safeAddress, to, nftID], nonce)
+	const call = buildContractCall(erc20, "transfer", [to, amount], nonce)
 	const tx = await executeTx(safeContract, call, signatures)
 	await tx.wait()
 }
