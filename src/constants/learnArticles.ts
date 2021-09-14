42b09aff69526e3c5a20b3f6eaab879151319e23
nginnever <ginneversource@gmail.com>
2021-09-14 07:21:21 -0700

    learn

--- src/constants/learnArticles.ts
+++ src/constants/learnArticles.ts
@@ -4,302 +4,150 @@ const LEARN_ARTICLES: Learn = [
 	{
 		title: "Introduction",
 		articles: [
-			`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-			`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-			`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-			`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-			`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-			`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-			`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-			`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+			`Welcome to the TokenWalk V0 documentation. These pages will thoroughly outline the TokenWalk ecosystem. If you are new here, we recommend starting with the high-level overview below.`,
+
+			`For step-by-step guides, jump to Getting Started.`,
+
+			`For developers interested in using our tools to create their own dApps or build upon Zodiac or TokenWalk, Jump to the developers sections and you can find all of our code here:`,
+			`https://github.com/TokenWalk`
+		],
+		childArticles: []
+	},
+	{
+		title: "What Is the TokenWalk dApp?",
+		articles: [
+			`TokenWalk is a dApp to help you boot up, list, and operate your DAO! We have collaborated with Gnosis Guild on building the Zodiac Module, and have developed the Seele Module to help you achieve an unstoppable organization.`,
+
+			`The TokenWalk dApp is catered to DAOs of all types, with the intention of building a vibrant ecosystem of communities that fosters new financial opportunities, collaborations, expression, and growth — implemented as mutually beneficial networks of DAOs.`
 		],
 		childArticles: [
 			{
-				title: "What Is TokenWalk?",
+				title: "Zodiac Modules",
 				articles: [
-					`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-					`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+					`Zodiac modules are at the core of your DAOs infrastructure. For more information on Zodiac [article link coming soon].`,
+
+					`With the TokenWalk dApp you will be able to choose to use all or some of the following Zodiac compliant modules and modifiers.`,
+
+					`Seele: allows avatars to operate with trustless tokenized DeGov, similar to Compound or Gitcoin, with a time-boxed proposal core that can register swappable voting contracts, allowing DAOs to choose from various on-chain voting methods that best suit their needs. (Built by TokenWalk)`,
+					`Exit: allows users to redeem a designated token for a relative share of an avatars assets, similar to Moloch's infamous rageQuit() function. (Built by Gnosis Guild)`,
+					`Bridge: allows an address on one chain to control an avatar on another chain, via an arbitrary message bridge. (Built by Gnosis Guild)`,
+					`Reality: allows on-chain execution based on the outcome of events reported by Reality.eth. Used to build the SafeSnap module for Snapshot. (Built by Gnosis Guild)`,
+					`More to come...`
+				],
+				childArticles: []
+			},
+			{
+				title: "Zodiac Modifiers",
+				articles: [
+					`Delay: allows avatars to enforce a time delay between when a module initiates a transaction and when it will be executed by the avatar. (Built by Gnosis Guild)`
 				],
 				childArticles: [
 					{
-						title: "Extension Modules",
+						title: "Voting Modifiers",
 						articles: [
-							`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
-						]
-					},
-					{
-						title: "Governance Modules",
-						articles: [
-							`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-							`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-							`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-							`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-							`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-							`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
-							`Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore 
-		magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
+							`Linear Voting ERC20 + delegation`,
+							`Linear Voting ERC20 + Membership`,
+							`Quadratic Voting ERC20 + Membership`,
+							`Single Voting`,
+							`Commitment ERC20 Voting`,
+
+							`We are building more modules and modifiers that extend the Zodiac ecosystem, such as exhibits and secure DeGov options to deal with issues like bribery. These novel extensions give rise to a new possibility of DAO implementations that allows for a progressive path to decentralization or to specific needs catered to any type of DAO.`
 						]
 					}
 				]
-			}
-		]
-	},
-	{
-		title: "Governance",
-		articles: [],
-		childArticles: [
-			{
-				title: "The TokenWalk DAO",
-				articles: [],
-				childArticles: [
-					{
-						title: "Core Pillars",
-						articles: []
-					}
-				]
 			},
 			{
-				title: "Gallery DAOs (Creator DAOs)",
-				articles: [],
-				childArticles: [
-					{
-						title: "Overview",
-						articles: []
-					},
-					{
-						title: "How Does Voting Work in a Gallery DAO?",
-						articles: []
-					},
-					{
-						title: "Gallery DAO Roles",
-						articles: []
-					},
-					{
-						title: "Examples of Gallery DAO Use Cases",
-						articles: []
-					}
-				]
-			},
-			{
-				title: "House DAOs (Finance DAOs)",
-				articles: [],
-				childArticles: [
-					{
-						title: "Overview",
-						articles: []
-					},
-					{
-						title: "How Does Voting Work in a House DAO?",
-						articles: []
-					},
-					{
-						title: "House DAO Roles",
-						articles: []
-					},
-					{
-						title: "Examples of House DAO Use Cases",
-						articles: []
-					}
-				]
-			}
-		]
-	},
-	{
-		title: "Protocol Overview",
-		articles: [],
-		childArticles: [
-			{
-				title: "How Does TokenWalk Work?",
-				articles: [],
+				title: "Gas Costs",
+				articles: [
+					`Gas costs present another challenge to large-scale on-chain voting, and examples exist for maintaining off-chain voting in a transparent way (e.g. Snapshot). While these solutions are cheap, they do introduce trust issues in the form of oracles. However, given that our dApp uses Zodiac Avatars as the asset manager core, our communities can continue to make use of SnapShot with the Reality module.`,
+
+					`While DAOs are in the early stages of development, there will likely not be many of the known issues present in DeGov. We believe that the best first step forward for DAOs moving to a cost effective trustless form of DeGov will be with delegation voting methods in the Seele module.`,
+
+					`If the user friction challenges are acceptable, DAOs can use Seele in conjunction with the Bridge module on a sidechain like xDAI. (Coming soon).`
+				],
 				childArticles: []
-			},
-			{
-				title: "TokenWalk's Ecosystem of DAO Networks",
-				articles: [],
-				childArticles: [
-					{
-						title: "Participants",
-						articles: []
-					}
-				]
-			},
-			{
-				title: "TokenWalk's DAO Modules",
-				articles: [],
-				childArticles: []
-			},
-			{
-				title: "Smart Contracts",
-				articles: [],
-				childArticles: [
-					{
-						title: "Gnosis Safe Core",
-						articles: []
-					},
-					{
-						title: "Gallery DAO Core Module",
-						articles: []
-					},
-					{
-						title: "House DAO Core Module",
-						articles: []
-					},
-					{
-						title: "Marketplace Extension",
-						articles: []
-					},
-					{
-						title: "Exhibit Extension Module",
-						articles: []
-					},
-					{
-						title: "Factory",
-						articles: []
-					}
-				]
 			}
 		]
 	},
 	{
-		title: "Getting Started",
-		articles: [],
-		childArticles: [
-			{
-				title: "NFTs",
-				articles: [],
-				childArticles: [
-					{
-						title: "How Do NFTs Work?",
-						articles: []
-					},
-					{
-						title: "What Is TokenWalk's Custom Domain Idea?",
-						articles: []
-					},
-					{
-						title: "Create NFTs",
-						articles: []
-					},
-					{
-						title: "Load Previously Created NFTs",
-						articles: []
-					},
-					{
-						title: "Edit NFTs",
-						articles: []
-					},
-					{
-						title: "What Is TokenWalk's Signing Extension?",
-						articles: []
-					}
-				]
-			},
-			{
-				title: "Gallery DAOs",
-				articles: [],
-				childArticles: [
-					{
-						title: "Set Up an ERC-20 Governance Token Gallery DAO",
-						articles: []
-					}
-				]
-			},
-			{
-				title: "House DAOs",
-				articles: [],
-				childArticles: [
-					{
-						title: "Set Up an ERC-20 Governance Token House DAO",
-						articles: []
-					},
-					{
-						title: "Set Up an ERC-721 Governance NFT House DAO",
-						articles: []
-					}
-				]
-			}
-		]
+		title: "What Is the TokenWalkDAO?",
+		articles: [
+			`TokenWalkDAO is committed to ensuring that creators can release their work as public goods, meaning that they no longer need to create paywalls, rely on secondary market speculators to provide unfair royalties, or treat their work like commodities. TokenWalkDAO is shifting the focus of digital content away from solely belonging to marketplaces or auction houses.`,
+			`To achieve this, we have structured TokenWalkDAO to be a retroactive public goods funding oracle similar to Optimism’s DAO. We have committed to using the TokenWalkDAOs treasury to support a landscape of creators that have shown a history of producing art, music, or any content that enriches humanity as a public good. This oracle provides a new possibility of museums of digital content — providing alternative solutions for how that content is funded, exhibited, as well as collected.`,
+			`TokenWalkDAO is a community dedicated to helping other DAOs boot up with the tools we have created with Gnosis. Feel free to join our discord if you have any questions or would like more general, technical, or legal help.`
+		],
+		childArticles: []
+	},
+	{
+		title: "Getting Started (Guides)",
+		articles: [`Coming Soon!`],
+		childArticles: []
 	},
 	{
 		title: "Developers",
-		articles: [],
+		articles: [`Coming Soon!`],
 		childArticles: [
 			{
+				title: "Smart Contracts",
+				articles: [`Coming Soon!`],
+				childArticles: [
+					{
+						title: "Avatars",
+						articles: [`Coming Soon!`]
+					},
+					{
+						title: "Seele Module",
+						articles: [`Coming Soon!`]
+					},
+					{
+						title: "Roles Module",
+						articles: [`Coming Soon!`]
+					},
+					{
+						title: "Delay Module",
+						articles: [`Coming Soon!`]
+					},
+					{
+						title: "Exit Module",
+						articles: [`Coming Soon!`]
+					},
+					{
+						title: "Reality Module",
+						articles: [`Coming Soon!`]
+					},
+					{
+						title: "Marketplace Extension Module",
+						articles: [`Coming Soon!`]
+					},
+					{
+						title: "Exhibit Extension Module",
+						articles: [`Coming Soon!`]
+					}
+				]
+			},
+			{
 				title: "ABI Files",
-				articles: [],
+				articles: [`Coming Soon!`],
 				childArticles: []
 			},
 			{
 				title: "Interfaces",
-				articles: [],
+				articles: [`Coming Soon!`],
 				childArticles: []
 			},
 			{
 				title: "Developer Tools",
-				articles: [],
+				articles: [`Coming Soon!`],
 				childArticles: []
 			},
 			{
 				title: "API",
-				articles: [],
+				articles: [`Coming Soon!`],
 				childArticles: []
 			},
 			{
 				title: "Deployment Specifics",
-				articles: [],
+				articles: [`Coming Soon!`],
 				childArticles: []
 			}
 		]
