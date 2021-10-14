ec7a022dfc4b82216214b6513f3e56c71d1bee79
Koochr <dm.girenko@gmail.com>
2021-10-14 23:36:49 +0100

    Refactor editing user profile

--- src/api/firebase/user/updateUserImage.ts
+++ src/api/firebase/user/updateUserImage.ts
@@ -1,4 +1,5 @@
 import firebase from "firebase"
+const {REACT_APP_CLOUD_FUNCTIONS_URL} = process.env
 
 const updateUserImage = async (
 	file: File,
@@ -8,11 +9,21 @@ const updateUserImage = async (
 	const imageRef = firebase.storage().ref(`users/${imageType}s/${account}`)
 	const snapshot = await imageRef.put(file)
 	const url = await snapshot.ref.getDownloadURL()
-	await firebase
-		.firestore()
-		.collection("users")
-		.doc(account.toLowerCase())
-		.update({[`${imageType}Image`]: url.split("&")[0]})
+	const token = await firebase.auth().currentUser?.getIdToken(true)
+	if (!token) {
+		throw new Error("Not authorized in firebase")
+	}
+	const res = await fetch(`${REACT_APP_CLOUD_FUNCTIONS_URL}/editUser`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+			authorization: `Bearer ${token}`
+		},
+		body: JSON.stringify({[`${imageType}Image`]: url.split("&")[0]})
+	})
+	if (res.status !== 200) {
+		throw new Error(`Failed to edit user ${imageType} image`)
+	}
 }
 
 export default updateUserImage
