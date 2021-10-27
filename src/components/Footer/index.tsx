1cd128ef779796689742384c54a14eadf6fdf42e
Kirill Klimenko <klimenkomud@gmail.com>
2021-10-27 05:18:17 +0300

    Update footer design

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,11 @@ import {Link} from "react-router-dom"
 import {ReactComponent as Logo} from "../../assets/icons/logo-white.svg"
 import {ReactComponent as DiscordGrayscale} from "../../assets/icons/discord-grayscale.svg"
 import {ReactComponent as TwitterGrayscale} from "../../assets/icons/twitter-grayscale.svg"
+import {ReactComponent as TelegramGrayscale} from "../../assets/icons/telegram-grayscale.svg"
+import {ReactComponent as GitHubGrayscale} from "../../assets/icons/github-grayscale.svg"
+import {ReactComponent as MediumGrayscale} from "../../assets/icons/medium-grayscale.svg"
 import "./styles.scss"
+import Divider from "../Divider"
 
 const Footer: FunctionComponent = () => {
 	return (
@@ -13,7 +17,26 @@ const Footer: FunctionComponent = () => {
 					<Link to="/">
 						<Logo width="80px" height="80px" />
 					</Link>
-					© Hyphal 2021
+				</div>
+				<div className="footer__col">
+					<h3 className="footer__text--dark">Projects</h3>
+					<a>HyphalDAO</a>
+					<a>Hyphal Galleries</a>
+					<a>Zodiac(Gnosis)</a>
+				</div>
+				<div className="footer__col">
+					<h3 className="footer__text--dark">Support</h3>
+					<a>Guidlines</a>
+					<a>Contact Us</a>
+					<a>Report Content</a>
+				</div>
+			</div>
+			<Divider />
+			<div className="footer__wrap">
+				<div className="footer__col">
+					<span className="footer__text--dark">© Hyphal 2021</span>
+				</div>
+				<div className="footer__col">
 					<div className="footer__socials">
 						<a target="_blank" rel="noopener noreferrer" href="https://twitter.com/hyphaldao">
 							<TwitterGrayscale width="24px" height="20px" />
@@ -21,20 +44,17 @@ const Footer: FunctionComponent = () => {
 						<a target="_blank" rel="noopener noreferrer" href="https://discord.gg/zC5CuFJwqu">
 							<DiscordGrayscale width="24px" height="20px" />
 						</a>
+						{/* TODO: Add link for Telegram once we will set it up. Keep icon for now to match the design */}
+						<a target="_blank" rel="noopener noreferrer">
+							<TelegramGrayscale width="24px" height="20px" />
+						</a>
+						<a target="_blank" rel="noopener noreferrer" href="https://github.com/HyphalDAO">
+							<GitHubGrayscale width="24px" height="20px" />
+						</a>
+						<a target="_blank" rel="noopener noreferrer" href="https://hyphal.medium.com/">
+							<MediumGrayscale width="24px" height="20px" />
+						</a>
 					</div>
-				</div>
-				<div className="footer__col">
-					<h3>Resources</h3>
-					<a>Learn</a>
-					<a>FAQ</a>
-					<a>Guidelines</a>
-					<a>Report Content</a>
-				</div>
-				<div className="footer__col">
-					<h3>Company</h3>
-					<a>Contact</a>
-					<a>Terms of Service</a>
-					<a>Privacy Policy</a>
 				</div>
 			</div>
 		</footer>
