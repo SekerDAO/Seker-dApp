6bdf502ea1ccbb4610fc8be5acffd29090ea3a30
Koochr <dm.girenko@gmail.com>
2021-10-29 01:23:31 +0100

    Update Dropdown

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -5,17 +5,31 @@ import "./styles.scss"
 
 const Dropdown: FunctionComponent<{
 	isOpened: boolean
-	items: {id: string; content: ReactElement | string}[]
-	triggerText: string
+	items: {id: string | number; content: ReactElement | string}[]
+	selected?: string | number | null
+	highlightSelected?: boolean
+	triggerText: string | number
 	onClose: () => void
 	onTriggerClick: () => void
 	onItemClick?: (itemId: string | number) => void
 	borders?: "all" | "none"
-}> = ({isOpened, onClose, items, onTriggerClick, onItemClick, triggerText, borders = "all"}) => {
+	className?: string
+}> = ({
+	isOpened,
+	onClose,
+	items,
+	onTriggerClick,
+	onItemClick,
+	triggerText,
+	className,
+	borders = "all",
+	selected,
+	highlightSelected
+}) => {
 	const ref = useRef<HTMLDivElement | null>(null)
 	useClickOutside(ref, onClose)
 
-	const handleItemClick = (id: string) => {
+	const handleItemClick = (id: string | number) => {
 		if (onItemClick) {
 			onItemClick(id)
 		}
@@ -36,7 +50,13 @@ const Dropdown: FunctionComponent<{
 				{isOpened && (
 					<>
 						{items.map(({id, content}) => (
-							<li key={id} onClick={() => handleItemClick(id)} className="dropdown__item">
+							<li
+								key={id}
+								onClick={() => handleItemClick(id)}
+								className={`dropdown__item${
+									highlightSelected && id === selected ? " dropdown__item--selected" : ""
+								}`}
+							>
 								{content}
 							</li>
 						))}
