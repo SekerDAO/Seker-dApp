98395bdad0c52ca0778ab57a332cf6b844b90a7e
Kirill Klimenko <klimenkomud@gmail.com>
2021-11-07 02:47:49 +0200

    More styles adjustments, remove unnecessary styles

--- src/components/Modals/ConfirmationModal/index.tsx
+++ src/components/Modals/ConfirmationModal/index.tsx
@@ -1,17 +1,16 @@
 import {FunctionComponent, useState} from "react"
 import Modal from "../Modal"
-import Button from "../../Controls/Button"
-import "./styles.scss"
 
 const ConfirmationModal: FunctionComponent<{
 	title: string
 	text: string
+	warningText: string
 	onSubmit: () => Promise<void>
 	submitText: string
 	cancelText?: string
 	isOpened: boolean
 	handleClose: () => void
-}> = ({title, text, onSubmit, submitText, cancelText, isOpened, handleClose}) => {
+}> = ({title, text, warningText, onSubmit, submitText, isOpened, handleClose}) => {
 	const [processing, setProcessing] = useState(false)
 
 	const handleSubmit = async () => {
@@ -22,17 +21,16 @@ const ConfirmationModal: FunctionComponent<{
 	}
 
 	return (
-		<Modal show={isOpened} onClose={handleClose}>
-			<h2 className="confirmation-modal__title">{title}</h2>
+		<Modal
+			show={isOpened}
+			onClose={handleClose}
+			title={title}
+			warningMessage={warningText}
+			submitButtonText={processing ? "Processing..." : submitText}
+			submitButtonDisabled={processing}
+			onSubmit={handleSubmit}
+		>
 			<p className="confirmation-modal__text">{text}</p>
-			<div className="confirmation-modal__buttons">
-				<Button buttonType="secondary" onClick={handleClose}>
-					{cancelText ?? "Cancel"}
-				</Button>
-				<Button onClick={handleSubmit} disabled={processing}>
-					{processing ? "Processing..." : submitText}
-				</Button>
-			</div>
 		</Modal>
 	)
 }
